05c04178fb0e22a45fe6de5ca086c9b9d486e854
Thomas K. Running <thomas@kjemperud.net>
2018-12-09 22:35:36 +0000

    added add task functionality

--- tasklist/app.js
+++ tasklist/app.js
@@ -1,6 +1,46 @@
 // Define UI Vars
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
-const clearBtn = ducument.querySelector('.clear-tasks');
+const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+    // add task event
+    form.addEventListener('submit', addTask);
+}
+
+// add task
+function addTask(e) {
+    if(taskInput.value === ''){
+        alert('Add a task');
+    }
+
+    // create li element
+    const li = document.createElement('li');
+    // add class
+    li.className = 'collection-item';
+    // create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // create new link element
+    const link = document.createElement('a');
+    // add class
+    link.className = 'delete-item secondary-content';
+    // add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // append link to li
+    li.appendChild(link);
+
+    // append li to ul
+    taskList.appendChild(li);
+    console.log(li)
+
+    // Clear input
+    taskInput.value = '';
+
+    e.preventDefault();
+}
